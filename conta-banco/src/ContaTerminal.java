92fc284b8d0ed79adf04780a1c8c662abedf558b
arilton0104 <ariltonfranco@gmail.com>
2024-01-02 16:43:12 -0300

    Conclusão do exercicio

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Declaração das variáveis
+        double saldo = 25;
+        double valorSolicitado = 18;
+
+        // Verificação do saldo
+        if (saldo > valorSolicitado) {
+            saldo = saldo - valorSolicitado;
+            System.out.println("Saldo atualizado: " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
+
+        // Segunda execução do programa
+        saldo = 15;
+        valorSolicitado = 22;
+
+        // Verificação do saldo
+        if (saldo > valorSolicitado) {
+            saldo = saldo - valorSolicitado;
+            System.out.println("Saldo atualizado: " + saldo);
+        } else {
+            System.out.println("Saldo insuficiente");
+        }
     }
 }
